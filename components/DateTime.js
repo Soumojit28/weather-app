2ab52cbce552d66a27fe6b59f679a1975e22c450
ericsilvertx <ericm@thesilvers.com>
2021-11-21 12:48:18 -0600

    Added %

--- components/DateTime.js
+++ components/DateTime.js
@@ -46,7 +46,7 @@ const DateTime = ({current, lat, lon, timezone}) => {
                    <Text style={styles.subheading}>{date}</Text>
                </View>
                <View style={styles.weatherItemContainer}>
-                    <WeatherItem title="Humidity" value={current? current.humidity : ""} unit="%"/>
+                    <WeatherItem title="Humidity %" value={current? current.humidity : ""} unit="%"/>
                     <WeatherItem title="Pressure" value={current? current.pressure : ""} unit="hPA"/>
                     <WeatherItem title="Sunrise" value={current? moment.tz(current.sunrise * 1000, timezone ).format('HH:mm'): ""} unit="am"/>
                     <WeatherItem title="Sunset" value={current? moment.tz(current.sunset * 1000, timezone ).format('HH:mm') : ""} unit="pm"/>
